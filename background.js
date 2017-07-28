0e44892371cc94c8726e6efc201e948efe6cd325
tstuber <tstuber@gmail.com>
2017-07-28 07:19:01 +0200

    Refactoring: now lookup results are provided by tooltip

--- background.js
+++ background.js
@@ -1,263 +1,179 @@
-﻿
-/**
- * Background logic for ipt chrome extension.
- */
-{
-  /**
-   * Global variables
-   */
-  var dictCc = {
-    "address": ".syn.dict.cc/?s=",
-    "language": "",
-    "direction": "",
-    "active": false,
-    "icon": "icon19.png",
-    "iconInactive": "icon19inactive.png",
-    "languageId": "language",
-    "directionId": "direction",
-    "urlExt": "&ref=chrome-ext",
-    "contextMenuSearch": "tooltip",
-    "storageContextMenuSearch": "dict_cc_context_menu_search",
-    "contextMenuSearchId": "contextmenusearch",
-    "storageActive": "dict_cc_active",
-    "searchInTextFieldId": "search_in_text_field",
-    "storageSearchInTextField": "dict_cc_search_in_text_field",
-    "searchInTextField": true,
-    "storageOpenInNewTab": "dict_cc_open_in_new_tab",
-    "openInNewTab": true,
-    "openInNewTabId": "open_in_new_tab",
-    "tooltipCenteredId": "tooltip_centered",
-    "tooltipCentered": false,
-    "storageTooltipCentered": "dict_cc_tooltip_centered",
-    "onlyDbClick": false,
-    "onlyDbClickId": "only_db_click",
-    "storageOnlyDbClick": "dict_cc_only_db_click"
-  };
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
+// Global variables only exist for the life of the page, so they get reset
+// each time the page is unloaded.
+var counter = 1;
+var lastTabId = -1;
+var employees = null;
 
-  var contextMenu = null;
-
-  /**
-   * Stick a listener for requests from other pages and browser action.
-   */
-  // chrome.runtime.onMessage.addListener(onMessage);
-
-  chrome.runtime.onMessage.addListener(onMessageTest);
-
-  function onMessageTest(request, sender, sendResponse) {
-    console.log(request);
-    console.log(sender.tab ?
-      "from a content script:" + sender.tab.url :
-      "from the extension");
-    if (request.greeting == "hello")
-      sendResponse({
-        farewell: "goodbye"
-      });
-  };
-
-
-  /**
-   * On startup, restore options or set defaults.
-   */
-  dictCc.contextMenuSearch = (localStorage[dictCc.storageContextMenuSearch] != null) ? localStorage[dictCc.storageContextMenuSearch] : dictCc.contextMenuSearch;
-  dictCc.searchInTextField = (localStorage[dictCc.storageSearchInTextField] != null) ? localStorage[dictCc.storageSearchInTextField] : dictCc.searchInTextField;
-  dictCc.openInNewTab = (localStorage[dictCc.storageOpenInNewTab] != null) ? localStorage[dictCc.storageOpenInNewTab] : dictCc.openInNewTab;
-  dictCc.tooltipCentered = (localStorage[dictCc.storageTooltipCentered] != null) ? localStorage[dictCc.storageTooltipCentered] : dictCc.tooltipCentered;
-  dictCc.onlyDbClick = (localStorage[dictCc.storageOnlyDbClick] != null) ? localStorage[dictCc.storageOnlyDbClick] : dictCc.onlyDbClick;
-
-
-  /**
-   * Create or remove the context menu search entry, based on the contextMenuSearch flag.
-   */
-  contextMenuSearch();
-
-  /**
-   * Simulate the button click, to set the correct tooltip based on the localization. The active flag must be inverted,
-   * because at the end of the onBrowserActionClicked function it will be inverted again.
-   */
-  onBrowserActionClicked(null);
-
-  /**
-   * Performs an XMLHttpRequest to dict.cc to get the translation of the
-   * user selection.
-   * @param user selection
-   * @param callback method on success.
-   */
-  function translateSelection(selection, callback) {
-    // Create request and define result function.
-    //
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function(data) {
-      // If the request was done, and the status is OK -> 200, invoke the callback function.
-      //
-      if (request.readyState == 4 && request.status == 200) {
-        callback(request.responseText);
+var lookup = function(searchTerm) {
+    // only if list of employees is loaded.
+    if (employees) {
+      for (var i = 0; i < employees.length; i++) {
+        if (employees[i].code == searchTerm) {
+          return employees[i];
+        }
       }
     }
+  },
 
-    // Build the translation url for the request.
-    //
-    var url = "http://" + dictCc.language + dictCc.address + selection + dictCc.urlExt;
-    console.log("dict-cc request url: " + url);
-
-    // Send the request to dict.cc.
-    //
-    request.open("GET", url, true);
-    request.send();
-  }
-
-  /**
-   * Handles data sent via chrome.extension.sendRequest().
-   * @param request object.
-   * @param sender object that invokes the request.
-   * @param callback function when the request completes.
-   */
-  function onMessage(request, sender, callback) {
-    console.log("background hit. ")
-    // If the translateSelection request was received, just call the method.
-    //
-    if (request.action == "translateSelection") {
-
-      // translateSelection(request.selection, callback);
-      callback("Dummy Employee")
-    }
-
-
-    // If the log request was received, just log the message.
-    //
-    if (request.action == "log") {
-      console.log(request.message);
-    }
-
-    // If resize was received, calculate and return the current window size.
-    //
-    if (request.action == "resize") {
-      chrome.windows.getCurrent(function(win) {
-        callback({
-          "width": win.width,
-          "height": win.height
-        });
-      });
-    }
-
-    // If the centered request was received, just return the tooltip centered flag.
-    //
-    if (request.action == "centered") {
-      callback(((dictCc.tooltipCentered == "true") || (dictCc.tooltipCentered == true)) ? true : false);
-    }
-  }
-
-
-  /**
-   * Returns a select element of all languages.
-   */
-  function getLanguageSelect() {
-    var select = document.createElement("select");
-
-    // Add german languages.
-    //
-    for (var key in languagesDE) {
-      var opt = document.createElement("option");
-      opt.innerHTML = languagesDE[key];
-      opt.value = key;
-
-      select.appendChild(opt);
-    }
-
-    // Add english languages.
-    //
-    for (var key in languagesEN) {
-      var opt = document.createElement("option");
-      opt.innerHTML = languagesEN[key];
-      opt.value = key;
-
-      select.appendChild(opt);
-    }
-
-    return select;
-  }
-
-
-  /**
-   * Return the detected language of the selected tab of the current window.
-   * @param callback for the detected language response.
-   */
-  function detectedLanguage(callback) {
-    chrome.windows.getCurrent(function(win) {
-      chrome.tabs.getSelected(win.id, function(tab) {
-        chrome.tabs.detectLanguage(tab.id, function(language) {
-          var l = null;
-
-          if (dictCc.direction == "de") {
-            for (var key in languagesDE) {
-              if (key.substr(2, 2) == language) {
-                l = {
-                  "key": key,
-                  "value": languagesDE[key].substr(12)
-                };
-                break;
-              }
-            }
-          } else {
-            for (var key in languagesEN) {
-              if (key.substr(2, 2) == language) {
-                l = {
-                  "key": key,
-                  "value": languagesEN[key].substr(12)
-                };
-                break;
-              }
-            }
-          }
-
-          // Callback the detected language.
-          //
-          callback(l);
-        });
-      });
-    });
-  }
-
-
-
-
-
-
-  /**
-   * Context menu function to translate a selected word, also available in frameset based websites.
-   */
-  function contextMenuTranslation(info, tab) {
-    if ((!info.selectionText) || (info.selectionText.length == 0)) {
-      return;
-    }
-
-    var max_len = 64;
-    var search_text = (info.selectionText.length <= max_len) ? info.selectionText : info.selectionText.substr(0, maxLength);
-
-    if (dictCc.contextMenuSearch == "tooltip" && ((dictCc.active == "true") || (dictCc.active == true))) {
-      // Send message to the context script.
-      chrome.tabs.getSelected(null, function(tab) {
-        chrome.tabs.sendMessage(tab.id, {
-          reason: "contextmenutranslation"
-        }, function() {});
-      });
-    } else {
-      // Open new tab for the translation.
-      var url = "http://" + dictCc.language + ".dict.cc/?s=" + search_text;
-      console.log("dict-cc context menu request url: " + url);
-      chrome.tabs.create({
-        "url": url
-      });
-    }
-  }
-
-
-  /**
-   * Log the current settings.
-   */
-  function logSettings(reason) {
-    console.log(reason + " language='" + dictCc.language + "' direction='" + dictCc.direction + "' meta='" + dictCc.metaModifier + "' shift='" + dictCc.shiftModifier + "' ctrl='" +
-      dictCc.ctrlModifier + "' alt='" + dictCc.altModifier + "' contextmenu='" + dictCc.contextMenuSearch + "' searchtextfield='" + dictCc.searchInTextField + "' active='" + dictCc.active + "'" +
-      " fontsize='" + dictCc.fontSize + "' newtab='" + dictCc.openInNewTab + "'" + "' centered='" + dictCc.tooltipCentered + "' onlydoubleclick='" + dictCc.onlyDbClick + "'");
+  // Function to read local employee json file.
+  xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    employees = JSON.parse(this.responseText);
   }
 }
+
+function sendMessage() {
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function(tabs) {
+    lastTabId = tabs[0].id;
+    console.log(lastTabId);
+    chrome.tabs.sendMessage(lastTabId, "Background page started.");
+  });
+}
+
+sendMessage();
+
+// Badge Text on browserAction.
+chrome.browserAction.setBadgeText({
+  text: "ON"
+});
+console.log("Loaded.");
+
+// chrome.runtime.onInstalled.addListener(function() {
+//   console.log("Installed.");
+//
+//   // localStorage is persisted, so it's a good place to keep state that you
+//   // need to persist across page reloads.
+//   localStorage.counter = 1;
+//
+//   // Register a webRequest rule to redirect bing to google.
+//   var wr = chrome.declarativeWebRequest;
+//   chrome.declarativeWebRequest.onRequest.addRules([{
+//     id: "0",
+//     conditions: [new wr.RequestMatcher({
+//       url: {
+//         hostSuffix: "bing.com"
+//       }
+//     })],
+//     actions: [new wr.RedirectRequest({
+//       redirectUrl: "http://google.com"
+//     })]
+//   }]);
+// });
+
+/*
+chrome.bookmarks.onRemoved.addListener(function(id, info) {
+  alert("I never liked that site anyway.");
+});
+*/
+
+
+/*
+// Wenn das browserAction Icon geklickt wird.
+chrome.browserAction.onClicked.addListener(function() {
+  // The event page will unload after handling this event (assuming nothing
+  // else is keeping it awake). The content script will become the main way to
+  // interact with us.
+  chrome.tabs.create({
+    url: "http://google.com"
+  }, function(tab) {
+    chrome.tabs.executeScript(tab.id, {
+      file: "content.js"
+    }, function() {
+      // Note: we also sent a message above, upon loading the event page,
+      // but the content script will not be loaded at that point, so we send
+      // another here.
+      sendMessage();
+    });
+  });
+});
+*/
+
+/*
+chrome.commands.onCommand.addListener(function(command) {
+  chrome.tabs.create({
+    url: "http://www.google.com/"
+  });
+});
+*/
+
+chrome.runtime.onMessage.addListener(function(msg, _, sendResponse) {
+  console.log("Listener hit!");
+  if (msg.setAlarm) {
+    // For testing only.  delayInMinutes will be rounded up to at least 1 in a
+    // packed or released extension.
+    chrome.alarms.create({
+      delayInMinutes: 0.1
+    });
+  } else if (msg.resize) {
+    sendResponse(chrome.windows.getCurrent(function(win) {
+      callback({
+        "width": win.width,
+        "height": win.height
+      });
+    }))
+  } else if (msg.delayedResponse) {
+    // Note: setTimeout itself does NOT keep the page awake. We return true
+    // from the onMessage event handler, which keeps the message channel open -
+    // in turn keeping the event page awake - until we call sendResponse.
+    setTimeout(function() {
+      sendResponse("Got your message.");
+    }, 5000);
+    return true;
+  } else if (msg.getCounters) {
+    sendResponse({
+      counter: counter++,
+      persistentCounter: localStorage.counter++
+    });
+  } else if (msg.getEmployee) {
+
+    // load list of employees.
+    if (employees == null) {
+      // load json data.
+      xmlhttp.open("GET", "iptEmployees.json", true);
+      xmlhttp.send();
+    }
+
+    // Lookup of employees.
+    obj = lookup(msg.code);
+
+    if (!obj) {
+      sendResponse();
+    } else {
+      sendResponse(obj);
+    }
+  }
+  // If we don't return anything, the message channel will close, regardless
+  // of whether we called sendResponse.
+});
+
+/*
+chrome.alarms.onAlarm.addListener(function() {
+  alert("Time's up!");
+});
+*/
+
+/*
+chrome.runtime.onSuspend.addListener(function() {
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function(tabs) {
+    // After the unload event listener runs, the page will unload, so any
+    // asynchronous callbacks will not fire.
+    alert("This does not show up.");
+  });
+  console.log("Unloading.");
+  chrome.browserAction.setBadgeText({
+    text: ""
+  });
+  chrome.tabs.sendMessage(lastTabId, "Background page unloaded.");
+});
+*/
---
+++ content.js
@@ -0,0 +1,246 @@
+// ipt Tooltip class.
+
+var iptTooltip = {
+
+  /*** Members ***/
+  tooltipID: {
+    "tooltip": "ipt_tooltip",
+    "content": "ipt_tooltip_content",
+  },
+  tooltipVisible: false,
+  cssLoaded: false,
+  employee: null,
+  currentWidth: 0,
+  currentHeight: 0,
+  currentLookup: null,
+  windowEvent: null,
+
+
+  /*** Methods ***/
+  init: function() {
+    // Query background page here; no stored variables yet.
+
+    iptTooltip.onWindowResize();
+    // Query backend initially.
+    iptTooltip.lookupCode(null);
+
+    window.addEventListener("resize", iptTooltip.onWindowResize);
+    document.addEventListener("dblclick", iptTooltip.onDbClick);
+    document.addEventListener("mouseup", iptTooltip.onMouseUp);
+
+    console.log("Ready to send a message to backend!");
+  },
+
+  // Provide easy access to element by id.
+  // @param id of the needed element.
+  $: function(id) {
+    return document.getElementById(id);
+  },
+
+  // If the window resize, determine the new window size.
+  onWindowResize: function() {
+    chrome.runtime.sendMessage({
+      resize: true
+    }, function(response) {
+      console.log(response);
+      if (response) {
+        console.log("width: " + response.width);
+        console.log("height: " + response.height);
+      }
+    })
+  },
+
+  onDbClick: function(evt) {
+    iptTooltip.onMouseUp(evt);
+  },
+
+  onMouseUp: function(evt) {
+    iptTooltip.windowEvent = evt;
+    console.log("mouse up click");
+
+    // Determine Tooltip position.
+    var range = window.getSelection().getRangeAt(0);
+    iptTooltip.setTooltipPosition(range);
+
+    // Selected string text.
+    var selection = (document.selection && document.selection.createRange().text) ||
+      (window.getSelection && window.getSelection().toString()).trim();
+
+    // Validate if selection is code.
+    if (/^[A-Z]{3}/.test(selection) && (iptTooltip.windowEvent.type == "dblclick")) {
+      iptTooltip.lookupCode(selection);
+    } else {
+      // Hide Tooltip if mouse presses somewhere else.
+      if (!iptTooltip.isChildFrom(iptTooltip.windowEvent.srcElement, iptTooltip.$(iptTooltip.tooltipID.tooltip))) {
+        iptTooltip.displayTranslationTooltip(false);
+        iptTooltip.removeTooltip();
+      }
+    }
+  },
+
+  lookupCode: function(selection) {
+
+    // save current lookup.
+    iptTooltip.currentLookup = selection;
+    console.log(selection);
+
+    // Query background page for name code.
+    chrome.runtime.sendMessage({
+      getEmployee: true,
+      code: selection
+    }, function(response) {
+      iptTooltip.fillDataToTooltip(response);
+    })
+  },
+
+  // get Range position method.
+  getRangePosition: function(range) {
+    var offset, left, top, right, bottom;
+
+    offset = range.getBoundingClientRect();
+
+    left = offset.left + document.body.scrollLeft;
+    top = offset.top + document.body.scrollTop;
+    right = offset.right + document.body.scrollLeft;
+    bottom = offset.bottom + document.body.scrollTop;
+
+    return {
+      "left": left,
+      "top": top,
+      "right": right,
+      "bottom": bottom
+    };
+  },
+
+  setTooltipPosition: function(range) {
+    var rangePosition = iptTooltip.getRangePosition(range);
+
+    var rangeLeft = rangePosition.left;
+    var rangeTop = rangePosition.top;
+    var rangeRight = rangePosition.right;
+    var rangeBottom = rangePosition.bottom;
+
+
+    // Build the tooltip if not exists.
+    //
+    var obj = iptTooltip.$(iptTooltip.tooltipID.tooltip);
+
+    if (obj == null) {
+      iptTooltip.createTooltip();
+      iptTooltip.displayTranslationTooltip(false);
+      obj = iptTooltip.$(iptTooltip.tooltipID.tooltip);
+    }
+
+    // do some positioning.
+    // Display below the selection.
+    obj.style.top = (rangeBottom + 10) + "px";
+    if (((rangeLeft + 360) > iptTooltip.currentWidth) && (rangeLeft > 360)) {
+      obj.style.left = (rangeRight - 360) + "px";
+    } else {
+      obj.style.left = (rangeLeft) + "px";
+    }
+  },
+
+  createTooltip: function() {
+    // Load the needed stylesheet.
+    //
+    if (!iptTooltip.cssLoaded) {
+      var stylesheet = document.createElement("link");
+      stylesheet.rel = "stylesheet";
+      stylesheet.type = "text/css";
+      stylesheet.href = chrome.extension.getURL("content.css");
+      document.head.appendChild(stylesheet);
+      iptTooltip.cssLoaded = true;
+    }
+
+    // Build the content container for the result table.
+    //
+    var tooltip_content = document.createElement("div");
+    tooltip_content.id = iptTooltip.tooltipID.content;
+
+    // Build the base container and add the elements.
+    //
+    var tooltip_container = document.createElement("div");
+    tooltip_container.id = iptTooltip.tooltipID.tooltip;
+    tooltip_container.appendChild(tooltip_content);
+
+    // Insert the tooltip to the page.
+    //
+    document.body.insertBefore(tooltip_container, document.body.firstChild);
+  },
+
+  // Remove the translation tooltip.
+  //
+  removeTooltip: function() {
+    var obj = iptTooltip.$(iptTooltip.tooltipID.tooltip);
+    if (obj != null) {
+      try {
+        obj.parentNode.removeChild(obj);
+      } catch (ex) {
+        console.log("Tooltip can not be removed. " + ex);
+      }
+    }
+  },
+
+  fillDataToTooltip: function(data) {
+
+    if (data) {
+      console.log(data);
+
+      var content_container = iptTooltip.$(iptTooltip.tooltipID.content);
+
+      var descNode = document.createElement("h3");
+      var descText = document.createTextNode(data.name + "(" + data.code + ")");
+      descNode.appendChild(descText);
+      content_container.appendChild(descNode);
+
+      var funcNode = document.createElement("p");
+      var funcText = document.createTextNode(data.function);
+      funcNode.appendChild(funcText);
+      content_container.appendChild(funcNode);
+
+      var pictureNode = document.createElement("img");
+      pictureNode.setAttribute("src", data.picture);
+      pictureNode.setAttribute("alt", data.name);
+      pictureNode.setAttribute("width", "320");
+      pictureNode.setAttribute("height", "240");
+
+      content_container.appendChild(pictureNode);
+
+      iptTooltip.displayTranslationTooltip(true);
+    }
+  },
+
+  // Show or hide the translation tooltip.
+  // @param visible state of the translation tooltip.
+  displayTranslationTooltip: function(visible) {
+    var obj = iptTooltip.$(iptTooltip.tooltipID.tooltip);
+
+    if (obj != null) {
+      if (visible) {
+        obj.style.display = "block";
+        obj.style.opacity = 1;
+        iptTooltip.tooltipVisible = true;
+      } else {
+        obj.style.display = "none";
+        iptTooltip.tooltipVisible = false;
+      }
+    }
+  },
+
+  // Determine if the given child is child node from the given element.
+  // @param child node
+  // @param element
+  isChildFrom: function(child, element) {
+    do {
+      if (child == element) {
+        return true;
+      }
+    }
+    while (child = child.parentNode);
+
+    return false;
+  }
+}
+
+iptTooltip.init();
